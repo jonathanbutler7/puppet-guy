7274ea3cd55cc538301c1b9d561a0563dce333b5
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-02-14 16:55:01 -0800

    adds jquery, image addresses and contact and videos pages

---
+++ index.js
@@ -0,0 +1,47 @@
+import { videos, contact } from './images/thumbnails/index.js';
+
+function selectContact() {
+  $('#contact').on('click', function () {
+    $('.contact').html(contact);
+    $('.contact').show();
+    $('.videos').hide();
+    $('#contact').addClass('selected');
+    $('#videos').removeClass('selected');
+    $('#about').removeClass('selected');
+    $('#puppics').removeClass('selected');
+  });
+}
+function selectVideos() {
+  $('#videos').on('click', function () {
+    $('.videos').html(videos);
+    $('.videos').show();
+    $('.contact').hide();
+    $('#videos').addClass('selected');
+    $('#about').removeClass('selected');
+    $('#puppics').removeClass('selected');
+    $('#contact').removeClass('selected');
+  });
+}
+function selectAbout() {
+  $('#about').on('click', function () {
+    $('#about').addClass('selected');
+    $('#videos').removeClass('selected');
+    $('#contact').removeClass('selected');
+    $('#puppics').removeClass('selected');
+  });
+}
+function selectPuppics() {
+  $('#puppics').on('click', function () {
+    $('#puppics').addClass('selected');
+    $('#videos').removeClass('selected');
+    $('#contact').removeClass('selected');
+    $('#about').removeClass('selected');
+  });
+}
+
+$(document).ready(() => {
+  selectVideos();
+  selectAbout();
+  selectPuppics();
+  selectContact();
+});
