8bceb2f5e190ba1ce16e079189d03f5423ac8c31
Jonathan Butler <jonathan.butler@getweave.com>
2021-03-27 21:25:46 -0700

    finishes puppics layout

--- store/pages/puppics.js
+++ store/pages/puppics.js
@@ -1,16 +1,10 @@
 import { puppicstxt, puppics } from '../constants/index.js';
 
-// const puppicsTxt = `
-//   <div class="puppics-text">
-//     <p>${puppicstxt}</p>
-//   </div>
-// `;
-
 const puppicsImgs = `
   <div style="display: flex;">
       <div class='puppics-images'>
         <div style="display: flex;">
-          <div style="flex: 1;">
+          <div style="flex: 1; padding: 0 1rem 1rem 0;">
             <img src=${puppics[0].img} alt=${puppics[0].alt}>
             <img src=${puppics[1].img} alt=${puppics[1].alt}>
           </div>
@@ -18,9 +12,9 @@ const puppicsImgs = `
             <img style="height: 85%" src=${puppics[2].img} alt=${puppics[2].alt}>
           </div>
         </div>
-        <img src=${puppics[3].img} alt=${puppics[3].alt}>
-        <img src=${puppics[4].img} alt=${puppics[4].alt}>
-        <img src=${puppics[5].img} alt=${puppics[5].alt}>
+        <img style="padding-bottom: 1rem;" src=${puppics[3].img} alt=${puppics[3].alt}>
+        <img style="padding-bottom: 1rem;" src=${puppics[4].img} alt=${puppics[4].alt}>
+        <img style="padding-bottom: 1rem;" src=${puppics[5].img} alt=${puppics[5].alt}>
       </div>
       <div class="puppics-text">
     <p>${puppicstxt}</p>
