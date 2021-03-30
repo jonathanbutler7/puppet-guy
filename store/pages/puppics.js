19e9ed8e1a527d38e9f39c16123130df64e745c8
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-03-29 21:19:41 -0700

    taket wo

--- store/pages/puppics.js
+++ store/pages/puppics.js
@@ -11,16 +11,16 @@ const puppicsImgs = `
             padding: 1rem;
             justify-content: space-between;
           ">
-            <img src=${puppics[0].img} alt=${puppics[0].alt}>
-            <img src=${puppics[1].img} alt=${puppics[1].alt}>
+            <img style="align-selft: center;" src=${puppics[0].img} alt=${puppics[0].alt}>
+            <img style="align-selft: center;" src=${puppics[1].img} alt=${puppics[1].alt}>
           </div>
           <div style="flex: 1;">
-            <img  src=${puppics[2].img} alt=${puppics[2].alt}>
+            <img style="align-selft: center;" src=${puppics[2].img} alt=${puppics[2].alt}>
           </div>
         </div>
-        <img style="padding-bottom: 1rem;" src=${puppics[3].img} alt=${puppics[3].alt}>
-        <img style="padding-bottom: 1rem;" src=${puppics[4].img} alt=${puppics[4].alt}>
-        <img style="padding-bottom: 1rem;" src=${puppics[5].img} alt=${puppics[5].alt}>
+        <img style="align-selft: center; padding-bottom: 1rem;" src=${puppics[3].img} alt=${puppics[3].alt}>
+        <img style="align-selft: center; padding-bottom: 1rem;" src=${puppics[4].img} alt=${puppics[4].alt}>
+        <img style="align-selft: center; padding-bottom: 1rem;" src=${puppics[5].img} alt=${puppics[5].alt}>
       </div>
       <div class="puppics-text">
     <p>${puppicstxt}</p>
